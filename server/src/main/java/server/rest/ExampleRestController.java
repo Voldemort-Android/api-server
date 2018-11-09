37b4a84a05474a9d20eedbcf45053c0f2f8d9b78
Unknown <alvinquach91@gmail.com>
2018-11-09 13:32:24 -0800

    oops

--- server/src/main/java/server/rest/ExampleRestController.java
+++ server/src/main/java/server/rest/ExampleRestController.java
@@ -3,8 +3,8 @@ package server.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -17,7 +17,7 @@ public class ExampleRestController {
 	}
 	
 	@GetMapping("/{param}")
-	public ResponseEntity<String> doesThisWork(@RequestParam String param) {
+	public ResponseEntity<String> doesThisWork(@PathVariable String param) {
 		return new ResponseEntity<String>("Param was '" + param + "'", HttpStatus.OK);
 	}
 

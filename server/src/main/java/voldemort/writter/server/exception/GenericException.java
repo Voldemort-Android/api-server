c85c4bc8c2cfd7290275bda5e63057dba7319b94
Unknown <alvinquach91@gmail.com>
2018-12-10 11:16:55 -0800

    added exceptions from landfill e-forms

---
+++ server/src/main/java/voldemort/writter/server/exception/GenericException.java
@@ -0,0 +1,19 @@
+package voldemort.writter.server.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Responds with a 500 Internal Server Error error code.
+ * @author Alvin Quach
+ */
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class GenericException extends RuntimeException {
+
+	private static final long serialVersionUID = -6343708373485172267L;
+
+	public GenericException(String message) {
+		super(message);
+	}
+	
+}

0b6bc2e329c3e5f62cd21c8116446bc89f76211d
Unknown <alvinquach91@gmail.com>
2018-12-13 04:26:08 -0800

    removed unused imports

--- server/src/main/java/voldemort/writter/server/ServerApplication.java
+++ server/src/main/java/voldemort/writter/server/ServerApplication.java
@@ -2,14 +2,6 @@ package voldemort.writter.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import voldemort.writter.server.security.AuthenticationHandlerInterceptor;
 
 /**
  * @author Alvin Quach
